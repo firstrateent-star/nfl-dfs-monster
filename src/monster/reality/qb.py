667ab2f655c0e58f4bb9b56e2beb056b6668fc8b
firstrateent-star <firstrateent@gmail.com>
2026-09-19 10:33:46 -0400

    v7: order QB taxonomy imports

--- src/monster/reality/qb.py
+++ src/monster/reality/qb.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from enum import StrEnum
-from collections.abc import Iterable
 
 from monster.sim.play_kernel import PassResult, PlayEvent, PlayType
 
